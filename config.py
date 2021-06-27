e277370a077c4def9cfce45593907859294b852f
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2021-06-27 18:57:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+version = "1.0.0"
+dev = "Kavin Bharathi - cipherDOT"
+web = "https://github.com/cipherDOT/nerdtree_project"
+help = "\n-v : shows the current version of nerdtree\n\n-auth : shows the developers of nerdtree\n\n-help : help command\n"
