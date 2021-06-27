8848b6ada1ef49300501a99016dfa3c5940ace90
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2021-06-27 17:56:19 +0530

    Update nerdtree.bat

--- nerdtree/nerdtree.bat
+++ nerdtree/nerdtree.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set path=%1
+set flag=%1
 
-C:\Users\Kavin\AppData\Local\Programs\Python\Python38\python.exe -u "C:\Users\Kavin\jarvis\nerdtree\tree.py" %path%
+C:\Users\Kavin\AppData\Local\Programs\Python\Python38\python.exe -u "C:\Users\Kavin\jarvis\nerdtree\tree.py" %flag%
+
 
