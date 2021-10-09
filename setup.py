bc040db11b0795a16f5d67971716cb5b36d7ee66
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2021-10-09 17:27:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 
-# version 2.2.0:
-#       [ ] added recursive locate command [see config and algos]
+# version 2.2.1:
+#       [ ] added error handling to invalid arguments
 
 # todo:
-#       [ ] make a commands file to organize the code
+#       [ ] improve usablity of code
 
 # --------------------------------------------imported libraries---------------------------------------------- #
 import sys
 import os
-from algos import locate, nerdtree, hidden_nerdtree
+from algos import locate, nerdtree, hidden_nerdtree, error_message
 from config import version, dev, web, help_cmd, use_cmd
 
 space = "    "
 
+# getting a list of all the usable commands for nerdtree
+commands = [x.split(':')[0].strip() for x in help_cmd]
+
 # --------------------------------getting the sys argument to provide the path-------------------------------- #
 
 try:
+    # figuring whether a first argument is given
     arg_1 = str(sys.argv[1])
 
     try:
+        # figuring whether a second argument is given
         arg_2 = str(sys.argv[2])
     except Exception:
         arg_2 = None
@@ -34,45 +39,52 @@ if arg_1 != None:
 
     if arg_2 != None:
 
-        # if "-la" is added with first argv, then we should take the path(argv[1]) and the hide{-ls} cmd(argv[2])
+        # if "-ls" is added with first argv, then we should take the path(argv[1]) and the hide{-ls} cmd(argv[2])
         if arg_2 == '-ls':
             # hidden nerdtree with the given path
             hidden_nerdtree(arg_1)
 
         if arg_1 == '-loc':
             locate(arg_2)
+
+        else:
+            error_message(arg_2)
     else:
-        # -v - version
-        if arg_1 == '-v':
-            print(f"{version}")
+        if arg_1 in commands:
+            # -v - version
+            if arg_1 == '-v':
+                print(f"{version}")
 
-        # "-dev" - name of developers
-        elif arg_1 == '-dev':
-            print(f"{dev}")
+            # "-dev" - name of developers
+            elif arg_1 == '-dev':
+                for i in dev:
+                    print(space + i)
 
-        # "-h" / "--help" - help section
-        elif arg_1 == "-h" or arg_1 == "--help":
-            print("COMMANDS")
-            for h in help_cmd:
-                print(space + h)
-            print("\n")
-            print("USAGE")
-            for u in use_cmd:
-                print(space + u)
+            # "-help" - help section
+            elif arg_1 == "-help":
+                print("COMMANDS")
+                for h in help_cmd:
+                    print(space + h)
+                print("\n")
+                print("USAGE")
+                for u in use_cmd:
+                    print(space + u)
 
-        # "-web" - shows the github page
-        elif arg_1 == "-web":
-            print(f"{web}")
+            # "-web" - shows the github page
+            elif arg_1 == "-web":
+                print(f"{web}")
 
-        # hidden nerdtree with the cwd path
-        elif arg_1 == "-ls":
-            hidden_nerdtree()
+            # hidden nerdtree with the cwd path
+            elif arg_1 == "-ls":
+                hidden_nerdtree()
 
-        # -loc without filename error handling
-        elif arg_1 == "-loc":
-            print("[ERROR] : give a name or a part of a name of a file to locate")
+            # -loc without filename [ error handling ]
+            elif arg_1 == "-loc":
+                print("[ERROR] : give a name or a part of a name of a file to locate")
 
-        # nerdtree runs at given location
+        elif arg_1[0] == '-':
+            error_message(arg_1)
+            # nerdtree runs at given location
         else:
             nerdtree(arg_1)
 else:
