7e53e8e3f599aa0b22855b2e30d6762b5915615c
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-03-25 13:51:24 +0530

    logo

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # configuration variables
 
 # current version
-version = "2.2.1"
+version = "alpha 3.0.0"
 
 # current devs
 dev = ["[developer]   == Kavin Bharathi {cipherDOT}",
-       "[github page] == github.com/cipherDOT"]
+        "[github page] == https://github.com/cipherDOT"]
 
 # github page for the project
 web = "https://github.com/cipherDOT/nerdtree"
@@ -15,15 +15,21 @@ help_cmd = [
     "-v    : shows the current version of nerdtree",
     "-dev  : shows the developers of nerdtree",
     "-help : displays the commands necessary to work with nerdtree",
-    "-ls   : list all directories without sub directories",
+    "-el   : list all directories with sub directories",
     "-web  : output the github repo for nerdtree project",
-    "-loc  : output the full path to the argument (file_name) if present in the cwd or in the dirs in cwd",
+    "-loc  : output the full path to the argument (file_name) if present in the cwd or in the dirs in cwd"
 ]
 
 # usage commands
 use_cmd = [
     "> nerdtree            : shows the file structure in the current working directory",
-    "> nerdtree -ls        : shows the file structure in cwd without the sub directories",
+    "> nerdtree -el        : shows the file structure in cwd with the sub directories",
     "> nerdtree [path]     : shows the file structure in the given working directory",
-    "> nerdtree [path] -ls : shows the file structure in given without the sub directories",
+    "> nerdtree [path] -el : shows the file structure in given with the sub directories",
+]
+
+# logo
+logo = [
+    " ._   _  ._ _| _|_ ._ _   _  ", 
+    " | | (/_ | (_|  |_ | (/_ (/_ "
 ]
