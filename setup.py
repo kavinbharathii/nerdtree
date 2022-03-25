d4a1ac15592d3bc6dc9c335780b8a845b4a974ec
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-03-25 13:52:22 +0530

    branch nerdtree

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 
-# version 2.2.1:
-#       [ ] added error handling to invalid arguments
+# version 2.2.0:
+#       [ ] added recursive locate command [see config and algos]
 
 # todo:
-#       [ ] improve usablity of code
+#       [ ] make a commands file to organize the code
 
 # --------------------------------------------imported libraries---------------------------------------------- #
 import sys
 import os
-from algos import locate, nerdtree, hidden_nerdtree, error_message
-from config import version, dev, web, help_cmd, use_cmd
+from algos import locate, nerdtree, branched_nerdtree
+from config import version, dev, web, help_cmd, use_cmd, logo
 
 space = "    "
-
-# getting a list of all the usable commands for nerdtree
-commands = [x.split(':')[0].strip() for x in help_cmd]
 
 # --------------------------------getting the sys argument to provide the path-------------------------------- #
 
 try:
-    # figuring whether a first argument is given
     arg_1 = str(sys.argv[1])
 
     try:
-        # figuring whether a second argument is given
         arg_2 = str(sys.argv[2])
     except Exception:
         arg_2 = None
@@ -39,56 +34,53 @@ if arg_1 != None:
 
     if arg_2 != None:
 
-        # if "-ls" is added with first argv, then we should take the path(argv[1]) and the hide{-ls} cmd(argv[2])
-        if arg_2 == '-ls':
-            # hidden nerdtree with the given path
-            hidden_nerdtree(arg_1)
+        # if "-la" is added with first argv, then we should take the path(argv[1]) and the branch{-el} cmd(argv[2])
+        if arg_2 == '-el':
+            # branched nerdtree with the given path
+            branched_nerdtree(arg_1)
 
         if arg_1 == '-loc':
             locate(arg_2)
-
-        else:
-            error_message(arg_2)
     else:
-        if arg_1 in commands:
-            # -v - version
-            if arg_1 == '-v':
-                print(f"{version}")
+        # -v - version
+        if arg_1 == '-v':
+            print(f"{version}")
 
-            # "-dev" - name of developers
-            elif arg_1 == '-dev':
-                for i in dev:
-                    print(space + i)
+        # "-dev" - name of developers
+        elif arg_1 == '-dev':
+            for i in dev:
+                print(space + i)
 
-            # "-help" - help section
-            elif arg_1 == "-help":
-                print("COMMANDS")
-                for h in help_cmd:
-                    print(space + h)
-                print("\n")
-                print("USAGE")
-                for u in use_cmd:
-                    print(space + u)
+        # "-h" / "--help" - help section
+        elif arg_1 == "-h" or arg_1 == "--help":
+            for logo_line in logo:
+                print(logo_line)
 
-            # "-web" - shows the github page
-            elif arg_1 == "-web":
-                print(f"{web}")
+            print()
+            print("COMMANDS")
+            for h in help_cmd:
+                print(space + h)
+            print("\n")
+            print("USAGE")
+            for u in use_cmd:
+                print(space + u)
 
-            # hidden nerdtree with the cwd path
-            elif arg_1 == "-ls":
-                hidden_nerdtree()
+        # "-web" - shows the github page
+        elif arg_1 == "-web":
+            print(f"{web}")
 
-            # -loc without filename [ error handling ]
-            elif arg_1 == "-loc":
-                print("[ERROR] : give a name or a part of a name of a file to locate")
+        # branched nerdtree with the cwd path
+        elif arg_1 == "-el":
+            branched_nerdtree()
 
-        elif arg_1[0] == '-':
-            error_message(arg_1)
-            # nerdtree runs at given location
+        # -loc without filename error handling
+        elif arg_1 == "-loc":
+            print("[ERROR] : give a name or a part of a name of a file to locate")
+
+        # nerdtree runs at given location
         else:
-            nerdtree(arg_1)
+            branched_nerdtree(arg_1)
 else:
     nerdtree()
-
 
 # -------------------------------------------------------------------------------------------------------------- #
