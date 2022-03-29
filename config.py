c9e870c89ff68d7175782dcafde35677278523d6
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-03-29 19:30:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ web = "https://github.com/cipherDOT/nerdtree"
 help_cmd = [
     "-v    : shows the current version of nerdtree",
     "-dev  : shows the developers of nerdtree",
-    "-help : displays the commands necessary to work with nerdtree",
+    "-h : displays the commands necessary to work with nerdtree",
     "-el   : list all directories with sub directories",
     "-web  : output the github repo for nerdtree project",
     "-loc  : output the full path to the argument (file_name) if present in the cwd or in the dirs in cwd"
